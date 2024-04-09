1b203e333341df4d2925eec55000c4f772a60cb3
Анастасия <114223955+nastuaah@users.noreply.github.com>
2024-04-09 10:54:15 +0300

    Update open3.py

--- open3.py
+++ open3.py
@@ -1,58 +1,55 @@
 import cv2
 import dlib
 
-# Load the pre-trained face landmark detection model from dlib
-predictor_path = "shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
 
-# Capture video from the webcam
+# Подключение видеопотока, вместо 0 можно вставить ссылку на видео
 cap = cv2.VideoCapture(0)
 
-# Initialize the lip press counter
+# Инициализация счетчика сжатых губ
 lip_press_count = 0
 
-# Flag to track if lips were previously pressed
+# Отслеживаем были ли ранее сжаты губы, чтобы считать только новые разы сжатия
 prev_lips_pressed = False
 
 while True:
     ret, frame = cap.read()
     
-    # Convert the frame to grayscale
+    # Преобразование кадров в оттенки серого
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    # Detect faces in the frame
+    # Поиск лиц на фрейме
     faces = detector(gray)
     
     for face in faces:
-        # Detect the facial landmarks
+        # Определение ориентиров на лице
         landmarks = predictor(gray, face)
         
-        # Extract the coordinates of the mouth region
+        # Извлечение координат области рта
         mouth_points = []
         for n in range(48, 68):
             x = landmarks.part(n).x
             y = landmarks.part(n).y
             mouth_points.append((x, y))
         
-        # Calculate the distance between upper and lower lip
+        # Подсчет дистанции между верхней и нижней губой
         lip_distance = mouth_points[15][1] - mouth_points[16][1]
         
-        # If the distance is less than a threshold and lips were not previously pressed, increment the counter
+        # Если расстояние меньше порогового и губы ранее не были сжаты, увеличиваем значение счетчика
         if lip_distance < 1 and not prev_lips_pressed:
             lip_press_count += 1
             prev_lips_pressed = True
         elif lip_distance >= 1:
             prev_lips_pressed = False
     
-    # Display the frame with annotations
+    # Отображение фрейма
     cv2.imshow('Lip Press Detection', frame)
     
-    # Break the loop if 'q' is pressed
+    # Завершение при нажатии q
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the video capture and close all windows
 print("Total lip presses: ", lip_press_count)
 cap.release()
 cv2.destroyAllWindows()
